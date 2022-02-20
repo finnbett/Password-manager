82a67ebfdd2da9c0790d6984d90ce3f0dc9abdf9
Finn <fluxr3@gmail.com>
2022-02-21 10:44:19 +1300

    canvas

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+from tkinter import *
+window = Tk()
+window.config(padx=20, pady=20)
+w = Canvas(window, width=200, height=200)
+logo = PhotoImage(file="logo.png")
+image = w.create_image(100, 100, image=logo)
+
+w.pack()
+window.mainloop()
