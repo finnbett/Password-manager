11d4f5b1b55c334046bb041469b8f1f9101202ea
Finn <fluxr3@gmail.com>
2022-02-21 11:52:11 +1300

    saves data

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def add_password():
+    #will write the data to a file,
+    f = open("data.txt", "a")
+    website_txt = website.get()
+    password_txt = password.get()
+    user_name_txt = user_name.get()
+    f.write(f"{website_txt} | {password_txt} | {user_name_txt}\n")
+    f.close()
+    #will clear entries
+    clear_entries()
+
+def clear_entries():
+    website.delete(0, END)
+    password.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 #image
 window = Tk()
@@ -19,6 +34,7 @@ website_label = Label(text="Website:")
 website_label.grid(column=1, row=2)
 user_name = Entry(width=35)
 user_name.grid(column=2, row=3, columnspan=2)
+user_name.insert(0, "example@gmail.com")
 user_name_label = Label(text="Username/email:")
 user_name_label.grid(column=1, row=3)
 password = Entry(width=21)
@@ -29,7 +45,7 @@ password_label.grid(column=1, row=4)
 #buttons
 generate_password = Button(text="Generate Password")
 generate_password.grid(row=4, column=3)
-add = Button(text="Add", width=36)
+add = Button(text="Add", width=36, command=add_password)
 add.grid(row=5, column=2, columnspan=2)
 
 
